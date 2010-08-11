1352cab21356be916d28754ae6435fc748d8e602
some.unique.login <some.unique.login@2e125067-c013-67ad-384d-d9d52c314671>
2010-08-11 19:50:29 +0000

    GWT gui: Add new game functionality

--- yat/src/main/java/net/sf/yat/gui/gwt/client/Entry.java
+++ yat/src/main/java/net/sf/yat/gui/gwt/client/Entry.java
@@ -4,18 +4,32 @@ import net.sf.yat.domain.Game;
 import net.sf.yat.domain.GameFactory;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Entry implements EntryPoint {
 
 	GameFactory gameFactory = new GameFactory(new HardCodedTaskDAO());
 	
-	public void onModuleLoad() {
-		Game game = gameFactory.generateGame(5, 5, 3);
-		MainView view = new MainView(game);
-		RootPanel.get().add(view);
-		
-		game.start();				
+	public void onModuleLoad() {		
+		final PopupPanel panel = new PopupPanel();
+		panel.setSize("150px", "150px");
+		final NewGameView newGame = new NewGameView();
+		newGame.btnOk.addClickHandler(new ClickHandler() {			
+			@Override
+			public void onClick(ClickEvent event) {				
+				Game game = newGame.getGame();
+				MainView view = new MainView(game);
+				RootPanel.get().add(view);
+				game.start();
+				panel.hide();
+			}
+		});
+		panel.add(newGame);
+		panel.setPopupPosition(0, 0);
+		panel.show();
 	}
 
 }
