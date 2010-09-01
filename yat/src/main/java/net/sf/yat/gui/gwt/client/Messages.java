2dc5645a6609f6c08809b3204bd55a990c137cd4
some.unique.login <some.unique.login@2e125067-c013-67ad-384d-d9d52c314671>
2010-09-01 18:33:26 +0000

    Initial changes on l10n

---
+++ yat/src/main/java/net/sf/yat/gui/gwt/client/Messages.java
@@ -0,0 +1,12 @@
+package net.sf.yat.gui.gwt.client;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface Messages extends Constants{
+	String tokenPause();
+	String tokenContinue();
+	String tokenGuessed();
+	String tokenFailed();
+	String tokenPlayer();
+	String tokenTeam();
+}
